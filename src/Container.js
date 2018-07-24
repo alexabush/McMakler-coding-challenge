432fe7cb1a3677c2db8bee8f8d11db4a4446c662
Alex Bush <alexabush7@gmail.com>
2018-07-24 14:43:49 -0700

    adding componentDidMount to get data from api"

--- src/Container.js
+++ src/Container.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
 import House from './House';
+import axios from 'axios';
+
 class Container extends Component {
   state = {
-    houses: [
-      {
-        name: 'Mieten',
-        tagline: 'adipisicing elit.',
-        subtag: 'Lorem ipsum, dolor sit amet consectetur',
-        price: 1.954,
-        address: '3 Zimmer',
-        space: 'ab 35 m^2'
-      },
-      {
-        name: 'Mieten',
-        tagline: 'adipisicing elit.',
-        subtag: 'Lorem ipsum, dolor sit amet consectetur',
-        price: 1.954,
-        address: '3 Zimmer',
-        space: 'ab 35 m^2'
-      },
-      {
-        name: 'Mieten',
-        tagline: 'adipisicing elit.',
-        subtag: 'Lorem ipsum, dolor sit amet consectetur',
-        price: 1.954,
-        address: '3 Zimmer',
-        space: 'ab 35 m^2'
-      }
-    ]
+    houses: []
+    // houses: [
+    //   {
+    //     name: 'Mieten',
+    //     tagline: 'adipisicing elit.',
+    //     subtag: 'Lorem ipsum, dolor sit amet consectetur',
+    //     price: 1.954,
+    //     address: '3 Zimmer',
+    //     space: 'ab 35 m^2'
+    //   },
+    //   {
+    //     name: 'Mieten',
+    //     tagline: 'adipisicing elit.',
+    //     subtag: 'Lorem ipsum, dolor sit amet consectetur',
+    //     price: 1.954,
+    //     address: '3 Zimmer',
+    //     space: 'ab 35 m^2'
+    //   },
+    //   {
+    //     name: 'Mieten',
+    //     tagline: 'adipisicing elit.',
+    //     subtag: 'Lorem ipsum, dolor sit amet consectetur',
+    //     price: 1.954,
+    //     address: '3 Zimmer',
+    //     space: 'ab 35 m^2'
+    //   }
+    // ]
   };
+
+  async componentDidMount() {
+    const rawData = await axios.get(
+      'https://api.mcmakler.de/v1/advertisements'
+    );
+    const data = rawData.data.data;
+    debugger;
+  }
 
   render() {
     const housesLis = this.state.houses.map(
