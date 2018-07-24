4a21ab3bd2d176ce2190c0415ad400189dea1d04
Alex Bush <alexabush7@gmail.com>
2018-07-24 14:16:27 -0700

    finished basic default styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 import Container from './Container';
 class App extends Component {
   render() {
--- src/House.js
+++ src/House.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import './House.css';
 
 class House extends Component {
   state = {};
   render() {
     return (
       <div className="House">
-        <h2>{this.props.name}</h2>
-        <img src="" alt="" />
-        <div>
-          <div>
+        <div className="House__container--image">
+          <h2 className="House__h2">{this.props.name}</h2>
+          <img className="House__img" src="" alt="" />
+        </div>
+        <div className="House__container--text">
+          <div className="House__container--tags">
             <h2>{this.props.tagline}</h2>
             <p>{this.props.subtag}</p>
           </div>
-          <div>
-            <p>{this.props.price}</p>
-            <div>
+          <div className="House__container--info">
+            <p>{this.props.price} &euro;</p>
+            <div className="House__container--additional-info">
               <p>{this.props.address}</p>
               <p>{this.props.space}</p>
             </div>
